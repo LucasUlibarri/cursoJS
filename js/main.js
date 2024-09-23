1cf4d515dfa6dc595c374d76887389165639c88e
Lucas Ulibarri <lucasulibarri26@gmail.com>
2024-09-23 01:00:34 -0300

    [M] Se hicieron cambios para la visualizacion de la lista de productos, sigue fallando el storage, si ajusto eso y puedo modificar el agregar carrito para que sume al local y agrego la funcion de orden superior que me falta definir donde iria, estaria lista la entrega 2

--- js/main.js
+++ js/main.js
@@ -22,41 +22,68 @@ let productos = [
     }
 ]
 
-/* for (const producto of productos){
+function listaProductos(){
+    for (const producto of productos){
+        let card = document.createElement('div');
+        card.className = 'cardProducto'
+        card.innerHTML = `<h3> ${producto.nombre} </h3>
+                            <p> ${producto.descripcion}</p>
+                            <b> $ ${producto.precio}</b>
+                            <button class="btnCarrito">Agregar al carrito</button>`
+        document.body.append(card)
+    }
+}
 
-    let card = document.createElement('div');
-    card.innerHTML = `<h3> ${producto.nombre} </h3>
-                        <p> ${producto.descripcion}</p>
-                        <b> $ ${producto.precio}</b>
-                        <button class="btnCarrito">Agregar al carrito</button>`
+    
 
-    document.body.append(card)
-} */
-
-/* function btnClick(x, y){
-    x = document.getElementById('"'+ x + '"');
-    x.addEventListener('click', y)
-} */
-
-
-let firstForm = div
-firstForm.className = "formContainer",
+let firstForm = document.createElement('form')
+firstForm.id = "formContainer",
 firstForm.innerHTML = `<input type="text" id="inputNombre" name="Nombre">
-                        <button id="firstBtn">Cargar</button>`;
+                        <input type="submit" id="firstBtn" value="Enviar">`;
 
 
 document.body.appendChild(firstForm);
 
 
-function storeName(){
+/* function storeName(){
     let inputNombre = document.getElementById("inputNombre");
     inputNombre.addEventListener('change', function(e){
     sessionStorage.setItem('Nombre', e.target.value);
 })
-}
+} */
 
+let inputNombre = document.getElementById("inputNombre");
 let btn = document.getElementById('firstBtn')
-btn.addEventListener("click", storeName)
+
+/* btn.addEventListener("click", () => {
+    inputNombre.addEventListener("change", (e)=>{
+        sessionStorage.setItem('nombre', e.target.value);
+    })
+}) */
+
+let validarForm = document.getElementById('formContainer')
+/* validarForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    inputNombre.addEventListener("change", (evento)=>{
+        sessionStorage.setItem('nombre', evento.target.value);
+    })
+}) */
+
+
+let nombreUsuario = sessionStorage.getItem('nombre')
+
+let nombreDisplay = div
+nombreDisplay.className = 'nombreDisplay';
+nombreDisplay.innerHTML = '<h1>Bienvenido ' + nombreUsuario + '!</h1>';
+
+btn.addEventListener("click", () => {    
+    inputNombre.addEventListener("change", (evento)=>{
+        sessionStorage.setItem('nombre', evento.target.value);
+    })
+    document.getElementById('formContainer').remove();
+    document.body.appendChild(nombreDisplay)
+    listaProductos()
+});
 
 
 //let firstBtn = document.getElementById('firstBtn');
